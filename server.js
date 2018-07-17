9a450267e56c2daff1c46f6cfde0643df2576650
Mariusz Szlachta <mariusz.szlachta@yahoo.com>
2018-07-17 21:33:55 +0200

    Change typo. Add semicolons

--- server.js
+++ server.js
@@ -2,25 +2,25 @@ var fs = require('fs');
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var strinifyFile;
+var stringifyFile;
 
 app.use(bodyParser.json());
 
 app.get('/getNote', function (req, res) {
   fs.readFile('./test.json', 'utf8', function(err, data) {
     if (err) throw err;
-    strinifyFile = data;
+    stringifyFile = data;
     res.send(data);
-  })
-})
+  });
+});
 
 app.post('/updateNote/:note', function(req, res){
   fs.readFile('./test.json', 'utf8', function(err, data){
-    strinifyFile = data + req.params.note;
-    fs.writeFile('./test.json', strinifyFile , function(err){
+    stringifyFile = data + req.params.note;
+    fs.writeFile('./test.json', stringifyFile , function(err){
       if (err) throw err;
       console.log('file uploaded')
-      res.send(strinifyFile);
+      res.send(stringifyFile);
     });
   });
 });
